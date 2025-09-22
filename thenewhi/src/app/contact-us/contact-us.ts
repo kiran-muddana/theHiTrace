2223cc2762c1a1ace9bf19f8b4cff78f135923a8
kiran -muddana <muddanasudhakiran@gmail.com>
2025-09-22 13:33:53 -0400

    added auto email

--- thenewhi/src/app/contact-us/contact-us.ts
+++ thenewhi/src/app/contact-us/contact-us.ts
@@ -4,17 +4,42 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [CommonModule,
-    FormsModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatButtonModule],
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatInputModule, MatFormFieldModule, MatButtonModule],
   templateUrl: './contact-us.html',
-  styleUrl: './contact-us.css'
+  styleUrls: ['./contact-us.css'],
 })
 export class ContactUs {
+  formData = {
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  };
 
+  onSubmit() {
+    console.log("form submitted..");
+    
+    emailjs
+      .send(
+        'service_aziowp4',
+        'template_wsnlusm',
+        {
+          from_name: this.formData.name,
+          from_email: this.formData.email,
+          phone: this.formData.phone,
+          message: this.formData.message,
+        },
+        'JsuWSyuO1NQSihs7S'
+      )
+      .then(() => {
+        alert('Form submitted! Confirmation email sent.');
+        this.formData = { name: '', email: '', phone: '', message: '' };
+      })
+      .catch((err) => console.error(err));
+  }
 }
